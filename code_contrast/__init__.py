af939d52616d6dc1a0823425f2fa6cd77ccf2508
mitya <dimitry.ageev@gmail.com>
2022-11-09 19:09:52 +0300

    add type hints and comtimue refactoring

--- code_contrast/__init__.py
+++ code_contrast/__init__.py
@@ -11,14 +11,15 @@ from typing import List
 
 
 class Encoding:
-    def __init__(self, name):
-        # TODO: tokens as properties
+    def __init__(self, name: str):
         self.DIAMOND = 0
         self.INFILL = 0
         self.ESCAPE = 0
         self.MSG = 0
         self.FILE = 0
         self.CHUNK = 0
+        self.LF = 0
+        self.EOT = 0
         self._pos_tokens = []
 
         filename = Path(__file__).resolve().parent.parent / "encodings" / f"{name}.json"
@@ -31,11 +32,11 @@ class Encoding:
             assert self.n_vocab == 50281
         elif name in ["openai_programming_v2", "openai_programming_v3"]:
             self.EOT = 50256
+            self.LF = 198
+            self.ESCAPE = self._encode_token(" §§")
             self._pos_tokens = list(range(50281, 50281 + 1024))
             assert self.decode([self._pos_tokens[0]]) == "<XXXXX>"
             assert self.decode([self._pos_tokens[-1]]) == "<VVVVV>"
-            self.ESCAPE = self._encode_token(" §§")
-            self.LF = 198  # TODO: only for this branch, other have no this attr
             LEAVE_LESS_TPOS = 256  # TODO: this must be an arg
             # for 2000 diffs n_ctx=2048, selftest fails LEAVE_LESS_TPOS=256 => 12, LEAVE_LESS_TPOS=512 => 0
             self._pos_tokens = self._pos_tokens[:LEAVE_LESS_TPOS]
@@ -54,6 +55,12 @@ class Encoding:
                 self.CHUNK = self._encode_token(" ►")
                 assert self.n_vocab == 50281 + 1024
         elif name in ['fb1', 'fb3']:
+            self.ESCAPE = 0
+            self.DIAMOND = 1
+            self.EOT = 2
+            self.MSG = self._encode_token("MSG")
+            self.FILE = self._encode_token("FILE")
+            self.CHUNK = self._encode_token("CH")
             if name == "fb1":
                 # Removed MASK tokens. Use for plain text.
                 assert self.n_vocab == 50261
@@ -63,13 +70,6 @@ class Encoding:
                 self._pos_tokens = list(range(50261, 50261 + 1024))
                 assert self.decode([self._pos_tokens[0]]) == "⪦XXXXX⪧"
                 assert self.decode([self._pos_tokens[-1]]) == "⪦VVVVV⪧"
-            self.ESCAPE = 0
-            self.DIAMOND = 1
-            self.EOT = 2
-            self.CHUNK = 3
-            self.MSG = self._encode_token("MSG")
-            self.FILE = self._encode_token("FILE")
-            self.CHUNK = self._encode_token("CH")
         elif name == 'facebook_incoder':
             # A useless encoding for our pursoses, don't use
             self.ESCAPE = None
@@ -78,28 +78,49 @@ class Encoding:
         else:
             assert 0
 
-    def _encode_token(self, d: str):
-        tokens = self.encode(d)
-        assert len(tokens) == 1, (d, tokens)
+    def _encode_token(self, text: str) -> int:
+        tokens = self.encode(text)
+        assert len(tokens) == 1, (text, tokens)
         return tokens[0]
 
     @property
-    def tpos(self):
+    def tpos(self) -> List[int]:
         return copy(self._pos_tokens)
 
     @property
-    def n_vocab(self):
+    def n_vocab(self) -> int:
         return self._tokenizer.get_vocab_size()
 
-    def is_tpos(self, token):
+    def is_tpos(self, token: int) -> bool:
         if not self._pos_tokens:
             return False
         return self._pos_tokens[0] <= token <= self._pos_tokens[-1]
 
-    def encode(self, s):
-        return self._tokenizer.encode(s).ids
+    def encode(self, sequence: str) -> List[int]:
+        return self._tokenizer.encode(sequence).ids
 
-    def decode(self, tokens, skip_zeros: bool = False, cut_at_eot: bool = False):
+    def encode_stochastic(self, sequence, bounds_at: List[int], prob: float) -> Tuple[List[int], List[int]]:
+        bounds_n = int(len(sequence) * prob)
+        if len(bounds_at) > 0:
+            assert bounds_at[0] == 0
+            assert bounds_at[-1] == len(sequence)
+            bounds_at = list(set(bounds_at))
+            bounds_at.sort()
+        else:
+            bounds_set = set([random.randint(0, len(sequence) - 1)
+                              for _ in range(bounds_n)])
+            bounds_set.add(len(sequence))
+            bounds_set.add(0)
+            bounds_at = list(bounds_set)
+            bounds_at.sort()
+        if len(bounds_at) == 1:  # set() eats equal elements, bad for zero-length strings
+            bounds_at = [0, len(sequence)]
+        result = []
+        for a, b in zip(bounds_at[:-1], bounds_at[1:]):
+            result.extend(self.encode(sequence[a:b]))
+        return result, bounds_at
+
+    def decode(self, tokens, skip_zeros: bool = False, cut_at_eot: bool = False) -> str:
         if isinstance(tokens, np.ndarray):
             assert len(tokens.shape) == 1, tokens.shape
         else:
@@ -184,24 +205,3 @@ class Encoding:
                               color=color, on_color=on_color)
             for (color, on_color), group in groupby(decode_colored(tokens, mask, diffedits), keyfunc)
         ])
-
-    def encode_stochastic(self, s, bounds_at: List[int], prob: float):
-        bounds_n = int(len(s) * prob)
-        if len(bounds_at) > 0:
-            assert bounds_at[0] == 0
-            assert bounds_at[-1] == len(s)
-            bounds_at = list(set(bounds_at))
-            bounds_at.sort()
-        else:
-            bounds_set = set([random.randint(0, len(s) - 1) for _ in range(bounds_n)])
-            bounds_set.add(len(s))
-            bounds_set.add(0)
-            bounds_at = list(bounds_set)
-            bounds_at.sort()
-        # print("bounds_at", bounds_at, " len(bounds_at)", len(bounds_at), " len(s)", len(s))
-        if len(bounds_at) == 1:  # set() eats equal elements, bad for zero-length strings
-            bounds_at = [0, len(s)]
-        result = []
-        for i in range(len(bounds_at)-1):
-            result.extend(self.encode(s[bounds_at[i] : bounds_at[i+1]]))
-        return result, bounds_at
