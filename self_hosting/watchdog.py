91ea95b0ffadcb24870b8485e170a86f694e35db
mitya <dimitry.ageev@gmail.com>
2022-12-08 18:16:44 +0300

    add watchdog

---
+++ self_hosting/watchdog.py
@@ -0,0 +1,117 @@
+import sys
+import time
+import logging
+import signal
+import subprocess
+
+from datetime import datetime
+from pathlib import Path
+
+from typing import Optional
+
+
+class Watchdog:
+
+    def __init__(self,
+                 host: str,
+                 port: int,
+                 workdir: Path,
+                 failed_upgrade_quit: bool):
+        self._host = host
+        self._port = port
+        self._workdir = workdir
+        self._failed_upgrade_quit = failed_upgrade_quit
+
+        self._quit_flag = False
+        self._package_url = "git+https://github.com/smallcloudai/code-contrast.git"
+
+        signal.signal(signal.SIGUSR1, self._catch_sigkill)
+
+    def _update_package(self) -> bool:
+        try:
+            subprocess.check_output(
+                [sys.executable, "-m", "pip", "install", "--upgrade", "--no-cache-dir", self._package_url],
+                stderr=subprocess.DEVNULL)
+            package_info = subprocess.check_output(
+                [sys.executable, "-m", "pip", "show", "code-contrast"],
+                stderr=subprocess.DEVNULL)
+            logging.info(f"package updated")
+            for info in package_info.decode("utf8").split("\n"):
+                logging.info(info)
+            return True
+        except subprocess.CalledProcessError as e:
+            logging.error(e)
+            return False
+
+    def _start_server(self) -> Optional[subprocess.Popen]:
+        try:
+            command = [
+                sys.executable,
+                "-m",
+                "self_hosting.server",
+                f"--host={self._host}",
+                f"--port={self._port}",
+                f"--workdir={self._workdir}",
+            ]
+            process = subprocess.Popen(
+                command,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.PIPE,
+            )
+            logging.info(f"server started pid={process.pid}")
+            return process
+        except ValueError as e:
+            logging.error(e)
+            return None
+
+    def _catch_sigkill(self, signum, frame):
+        logging.info("caught SIGUSR1")
+        self._quit_flag = True
+
+    def run(self):
+        while not self._quit_flag:
+            successful = self._update_package()
+            if self._failed_upgrade_quit and not successful:
+                break
+            process = self._start_server()
+            while True:
+                if self._quit_flag:
+                    process.kill()
+                    logging.info(f"server is shutting down pid={process.pid}")
+                    process.wait()
+                    break
+                retcode = process.poll()
+                if retcode is not None:
+                    logging.info(f"server exited with {retcode} pid={process.pid}")
+                    break
+                logging.info(f"server is working pid={process.pid}")
+                time.sleep(10)
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=8008)
+    parser.add_argument("--workdir", type=Path, default=Path("/working_volume"))
+    parser.add_argument("--failed-upgrade-quit", action="store_true")
+    args = parser.parse_args()
+
+    logdir = args.workdir / "logs"
+    logdir.mkdir(exist_ok=True, parents=False)
+    file_handler = logging.FileHandler(filename=logdir / f"watchdog_{datetime.now():%Y-%m-%d-%H-%M-%S}.log")
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    logging.basicConfig(level=logging.INFO, handlers=[stream_handler, file_handler])
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    watchdog = Watchdog(
+        host=args.host,
+        port=args.port,
+        workdir=args.workdir,
+        failed_upgrade_quit=args.failed_upgrade_quit,
+    )
+    watchdog.run()
