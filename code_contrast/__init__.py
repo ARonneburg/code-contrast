29c2baf5583efa24363025fb7ef409cc3dae0bf8
Oleg Klimov <omgtech@gmail.com>
2023-05-19 09:22:11 +0200

    dont "import *"

--- code_contrast/__init__.py
+++ code_contrast/__init__.py
@@ -5,5 +5,7 @@ from code_contrast.print_utils import editclass_print
 
 from code_contrast.contrast import *
 from code_contrast.scratchpad import *
-from code_contrast.modeling import *
 from code_contrast.contrast_2023q2 import *
+
+# Modeling has dependencies, bad idea to import it by default
+#from code_contrast.modeling import *
